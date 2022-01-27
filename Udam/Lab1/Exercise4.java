a37d07ea29a00c4e76cfb4489932a61f4dc59817
raychannudam <62501505+raychannudam@users.noreply.github.com>
2022-01-27 10:28:59 +0700

    Lab1 - move to folder Udam

---
+++ Udam/Lab1/Exercise4.java
@@ -0,0 +1,22 @@
+package Udam.Lab1;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        short a = 1;
+        short b = 2;
+        short c;
+
+        System.out.println("Before swap");
+        System.out.println("a : " + a + ", b: " + b);
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("After swap");
+        System.out.println("a : " + a + ", b: " + b);
+        
+    }
+}
+
+
---
+++ Udam/Lab1/Exercise8.java
@@ -0,0 +1,12 @@
+package Udam.Lab1;
+
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Type whatever you want: ");
+        String userInput = scan.nextLine();
+        System.out.println(userInput);
+    }
+}
