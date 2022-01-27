1a930be98ec23a35abec8cd4028152fb8f0e272b
raychannudam <62501505+raychannudam@users.noreply.github.com>
2022-01-27 10:24:37 +0700

    lab1 - ex10

--- Lab1/Exercise10.java
+++ Lab1/Exercise10.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Exercise10 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter radious: ");
-        float radius = scan.nextFloat();
-        System.out.println("Area = " + ((radius * radius) * 3.14));
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Enter radious: ");
+            float radius = scan.nextFloat();
+            System.out.println("Area = " + ((radius * radius) * 3.14));
+        }
 
     }
 }
