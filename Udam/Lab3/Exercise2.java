0654f4484d557fb5d3cbef316729f637e8db59f7
raychannudam <62501505+raychannudam@users.noreply.github.com>
2022-02-09 22:32:43 +0700

    Lab3 Exercise1-5

---
+++ Udam/Lab3/Exercise2.java
@@ -0,0 +1,14 @@
+package Udam.Lab3;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int counter = 1;
+        for(int i=0; i<20; i++){
+            for(int j=0; j<5; j++){
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
